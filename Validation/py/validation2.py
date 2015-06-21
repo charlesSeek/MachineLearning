39077bc8428a1220e2133c597c1e29f77f0f336d
shucheng cui <cuishucheng@gmail.com>
2015-06-21 23:06:05 +1000

    add Validation2.py code

---
+++ Validation/py/validation2.py
@@ -0,0 +1,74 @@
+import numpy as np
+def load_data(file):
+	try:
+		matrix = []
+		f = open(file,'r')
+		for line in f.readlines():
+			x = list(map(float,line.split()))
+			x.insert(0,1.0)
+			matrix.append(x)
+	finally:
+		if f:
+			f.close()
+	return matrix
+
+def sign(n):
+	if n <=0:
+		return -1
+	else:
+		return 1
+
+def calErrRate(output,predict):
+	errCount = 0
+	for i in range(len(output)):
+		if output[i]!=predict[i]:
+			errCount = errCount + 1
+	return errCount/len(output)
+
+def minError(data):
+	minError = 1
+	for i in data:
+		if i[1] < minError:
+			minError = i[1]
+			coef = i[0]
+			weight = i[2]
+	return coef,minError,weight
+
+coef = [100,10,1,0.1,0.01,0.001,0.0001,0.00001,0.000001,0.0000001,0.00000001,0.000000001,0.0000000001]
+data = load_data('train.txt')
+train = data[:120]
+validation = data[120:]
+test = load_data('test.txt')
+colNum = len(data[0])
+
+trainx =[]
+trainy=[]
+for i in train:
+	trainx.append(i[:colNum-1])
+	trainy.append(i[colNum-1])
+validationx = []
+validationy = []
+for i in validation:
+	validationx.append(i[:colNum-1])
+	validationy.append(i[colNum-1])
+testx =[]
+testy=[]
+for i in test:
+	testx.append(i[:colNum-1])
+	testy.append(i[colNum-1])
+
+dataVal= []
+for p in coef:
+	weight = np.dot(np.dot(np.linalg.inv(np.add(np.dot(np.transpose(trainx),trainx),np.dot(p,np.identity(colNum-1)))),np.transpose(trainx)),trainy)
+	coefIn = []
+	coefIn.append(p)
+	coefIn.append(calErrRate(validationy,list(map(sign,np.dot(weight,np.transpose(validationx))))))
+	coefIn.append(weight)
+	dataVal.append(coefIn)
+coef,rate,w = minError(dataVal)
+print('the coef:',coef)
+print('the error rate in train:',calErrRate(trainy,list(map(sign,np.dot(w,np.transpose(trainx))))))
+print('the error rate in validation:',rate)
+print('the error rate in test:',calErrRate(testy,list(map(sign,np.dot(w,np.transpose(testx))))))
+
+
--- Validation/readme.txt
+++ Validation/readme.txt
@@ -1,8 +1,9 @@
 The implementation of machine learning validation
 
 1. validation:
-example:
-ow split the given training examples in  to the first 120 examples for train and 80 for val. 
-Ideally, you should randomly do the 120/80 split. Because the given examples are already randomly permuted, however, we would use a fixed split for the purpose of this problem. 
-Run the algorithm on train to get g−λ, and validate g−λ with val.
+split the given training examples in to the first 120 examples for train and 80 for val. Ideally, you should randomly do the 120/80 split. Because the given examples are already randomly permuted, however, we would use a fixed split for the purpose of this problem. Run the algorithm on train to get g−λ, and validate g−λ with val.
 Among log10λ={2,1,0,−1,…,−8,−9,−10}. What is the λ with the minimum Etrain(g−λ)? Compute λ and the corresponding Etrain(g−λ), Eval(g−λ) and Eout(g−λ) then select the closet answer. Break the tie by selecting the largest λ.
+
+2.validation2
+Among log10λ={2,1,0,−1,…,−8,−9,−10}. What is the λ with the minimum Eval(g−λ)? Compute λ and the corresponding Etrain(g−λ), Eval(g−λ) and Eout(g−λ) then select the closet answer. Break the tie by selecting the largest λ.
+
